7de8833fdbd272c8250acfb6841d0ad8fa402758
Ashraful229 <emonak52@gmail.com>
2022-01-30 16:34:39 +0600

    JWT Done

--- jwt-example/src/main/java/com/jwt/jwtexample/config/SecurityConfig.java
+++ jwt-example/src/main/java/com/jwt/jwtexample/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.jwt.jwtexample.config;
 
+import com.jwt.jwtexample.filter.JwtFilter;
 import com.jwt.jwtexample.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +12,17 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private JwtFilter filter;
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
     @Override
@@ -38,7 +43,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.csrf().disable().authorizeHttpRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
+       http.csrf().disable().authorizeHttpRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated().and().exceptionHandling().and().sessionManagement()
+               .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+       http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
     }
 
 }
